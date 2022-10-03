c761bf192190af88d3f615c348b91c5a57ff79c0
christien07 <christien0101@protonmail.com>
2022-10-02 18:33:50 -0700

    Add array to temp. store generated password

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ def main():
     num_letters = input("How many letters would you like in your password?\n")
     num_symbols = input("How many symbols would you like in your password?\n")
     num_numbers = input("How many numbers would you like in your password?\n")
+
+    # Store gen. password in array
+    passwd = []
     
+
 
 if __name__ == "__main__":
     main()
