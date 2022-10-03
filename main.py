2c3be1076851591f127ca12f497715854705a41a
christien07 <christien0101@protonmail.com>
2022-10-02 18:31:01 -0700

    Add user input for number of numbers

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ def main():
     print("Welcome to my Password Generator Tool built in Python.")
     num_letters = input("How many letters would you like in your password?\n")
     num_symbols = input("How many symbols would you like in your password?\n")
+    num_numbers = input("How many numbers would you like in your password?\n")
+    
 
 if __name__ == "__main__":
     main()
