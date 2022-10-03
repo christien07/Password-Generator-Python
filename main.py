5fb8418fe5d0b86b5d90e408c0071b43a8a93d7e
christien07 <christien0101@protonmail.com>
2022-10-02 18:29:03 -0700

    Add initial user input

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 def main():
     print("Welcome to my Password Generator Tool built in Python.")
+    num_letters = input("How many letters would you like in your password?\n")
+    
 
 if __name__ == "__main__":
     main()
