8cf2eb84c976ec1fb6b4da4223605aa4ac5aa17f
christien07 <christien0101@protonmail.com>
2022-10-02 18:30:24 -0700

    Add user input for number of symbols

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
     print("Welcome to my Password Generator Tool built in Python.")
     num_letters = input("How many letters would you like in your password?\n")
-    
+    num_symbols = input("How many symbols would you like in your password?\n")
 
 if __name__ == "__main__":
     main()
