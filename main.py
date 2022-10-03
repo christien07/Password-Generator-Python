c0c92b041bbc0d326de3ffda75639278072f3120
christien07 <christien0101@protonmail.com>
2022-10-02 18:49:24 -0700

    Add option for security-minded users

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+import secrets
+
 def main():
     print("Welcome to my Password Generator Tool built in Python.")
-    num_letters = input("How many letters would you like in your password?\n")
-    num_symbols = input("How many symbols would you like in your password?\n")
-    num_numbers = input("How many numbers would you like in your password?\n")
+    security_measure = input("Does this password need to be reasonably secure? Y or N\n")
+    if security_measure = 'Y':
 
-    # Store gen. password in array
-    passwd = []
-    
+    elif security_measure = 'N':
+        num_letters = input("How many letters would you like in your password?\n")
+        num_symbols = input("How many symbols would you like in your password?\n")
+        num_numbers = input("How many numbers would you like in your password?\n")
+
+        # Store gen. password in array
+        passwd = []
+
 
 
 if __name__ == "__main__":
